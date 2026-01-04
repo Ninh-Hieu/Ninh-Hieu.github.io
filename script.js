100057e6d138ec3270062b549d24b575dbdf26fa
Ninh-Hieu <duchieuyn@gmail.com>
2026-01-04 22:01:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Cấu hình danh sách tiền và tỉ lệ (GIỮ NGUYÊN)
 const prizeData = [
-    { value: 10000, img: 'images/10k.png', elementId: 'chk-10k', weight: 20 },
+    { value: 10000, img: 'images/10k.png', elementId: 'chk-10k', weight: 15 },
     { value: 20000, img: 'images/20k.png', elementId: 'chk-20k', weight: 70 },
     { value: 50000, img: 'images/50k.png', elementId: 'chk-50k', weight: 15 },
     { value: 100000, img: 'images/100k.png', elementId: 'chk-100k', weight: 2 },
